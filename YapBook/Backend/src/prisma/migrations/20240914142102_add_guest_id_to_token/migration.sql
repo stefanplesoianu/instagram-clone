4dbf67e9c3ed18440503c7dea046422ff241a822
stefan.plesoianu <stefan.plesoianu@yahoo.com>
2024-11-30 14:31:25 +0200

    Removed old Backend folder files

--- YapBook/Backend/src/prisma/migrations/20240914142102_add_guest_id_to_token/migration.sql
+++
@@ -1,6 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Token" DROP CONSTRAINT "Token_userId_fkey";
-
--- AlterTable
-ALTER TABLE "Token" ADD COLUMN     "guestId" TEXT,
-ALTER COLUMN "userId" DROP NOT NULL;
--- YapBook/Backend/src/verifyToken.js
+++
@@ -1,5 +0,0 @@
-const passport = require('passport');
-
-const verifyToken = passport.authenticate('jwt', { session: false });
-
-module.exports = verifyToken;
